266ea2a7570e4364f5d4a89135a39681ff0474e5
patrick <patrickchow0803@gmail.com>
2020-02-09 21:57:29 -0500

    Added an if statement to prevent going out of bounds with the questions

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -23,50 +23,63 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText': "What\s your favorite color?",
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': "What\s your favorite animal?",
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+    },
+    {
+      'questionText': "Who is your favorite teacher?",
+      'answers': ['Max', 'Titly']
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     void _answerQuestion() {
+      // questions contains an array of maps
+      // const = compile time constant
+
       // use the function setState for code that will change data and will update the ui
       setState(() {
         _questionIndex = _questionIndex + 1;
       });
       print(_questionIndex.toString());
+
+      if (_questionIndex < questions.length) {
+        print('We have more questions!');
+      }
     }
 
-    // questions contains an array of maps
-    var questions = [
-      {
-        'questionText': "What\s your favorite color?",
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': "What\s your favorite animal?",
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
-      },
-      {
-        'questionText': "Who is your favorite teacher?",
-        'answers': ['Max', 'Titly']
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('This is my first app'),
         ),
-        body: Column(
-          children: [
-            // Acesses the questions variable with the index _questionIndex with the key as the string
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            // These lines of code is run for each item in the list. For each item, create an Answer widget.
-            ...(questions[_questionIndex]['answers'] as List<String>)
-            // answer is the string value of the list obtained within the map.
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        // _questionIndex < questions.length is an if statement. Perform everything after the ? if its true.
+        // Else perform everything after the Column.
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  // Acesses the questions variable with the index _questionIndex with the key as the string
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  // These lines of code is run for each item in the list. For each item, create an Answer widget.
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      // answer is the string value of the list obtained within the map.
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You Did it!'),
+              ),
       ),
     );
   }
